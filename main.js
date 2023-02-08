9472939124000882c30b7a26416080e5c4f34796
unknown <you@example.com>
2023-02-08 10:41:04 +0200

    add JS for separate each

---
+++ main.js
@@ -0,0 +1,25 @@
+// function called from ID
+let tempAmount =0;
+let checkAmount=document.getElementById("check-amount");
+let checkIncome =document.getElementById("check-income");
+
+// const are not change for button
+const checkAmountButtom=document.getElementById("check-amount");
+const checkIncomeButtom=document.getElementById("check-income");
+
+//input text
+const productTitle=document.getElementById("product-title");
+const userAmount=document.getElementById("user-amount");
+
+// error value
+const productTitleError=document.getElementById("product-title-error");
+
+// Output for expense
+const expenditureValue=document.getElementById("expenditure-value");
+const incomeValue =document.getElementById("income-value");
+const balanceAmount =document.getElementById("balance-amount");
+
+// List display 
+const list=document.getElementById("list");
+
+
